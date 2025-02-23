61c2f3f5c882b9f7e87a5bd47783bd41d4b53241
Autousername <cameron.belcher@developersinstitute.co.nz>
2025-02-23 19:43:38 +1300

    Completed studying/doing the classification project with the Custom Vision SDK, but the free tier for azure only allows 2 projects, which this exceeds

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,102 @@ const predictionEndpoint = process.env["VISION_PREDICTION_ENDPOINT"];
 
 const publishIterationName = "classifyModel";
 const setTimeoutPromise = util.promisify(setTimeout);
-console.log(setTimeoutPromise);
+
+const credentials = new msRest.ApiKeyCredentials({
+  inHeader: { "Training-key": trainingKey },
+});
+const trainer = new TrainingApi.TrainingAPIClient(
+  credentials,
+  trainingEndpoint
+);
+
+const predictor_credentials = new msRest.ApiKeyCredentials({
+  inHeader: { "Prediction-key": predictionKey },
+});
+const predictor = new PredictionApi.PredictionAPIClient(
+  predictor_credentials,
+  predictionEndpoint
+);
+
+(async () => {
+  console.log("Creating project...");
+  const sampleProject = await trainer.createProject("Sample Project");
+
+  const hemlockTag = await trainer.createTag(sampleProject.id, "Hemlock");
+  const cherryTag = await trainer.createTag(
+    sampleProject.id,
+    "Japanese Cherry"
+  );
+
+  const sampleDataRoot = "Images";
+
+  console.log("Adding images...");
+  let fileUploadPromises = [];
+
+  const hemlockDir = `${sampleDataRoot}/Hemlock`;
+  const hemlockFiles = fs.readdirSync(hemlockDir);
+  hemlockFiles.forEach((file) => {
+    fileUploadPromises.push(
+      trainer.createImagesFromData(
+        sampleProject.id,
+        fs.readFileSync(`${hemlockDir}/${file}`),
+        { tagIds: [hemlockTag.id] }
+      )
+    );
+  });
+
+  const cherryDir = `${sampleDataRoot}/Japanese_Cherry`;
+  const japaneseCherryFiles = fs.readdirSync(cherryDir);
+  japaneseCherryFiles.forEach((file) => {
+    fileUploadPromises.push(
+      trainer.createImagesFromData(
+        sampleProject.id,
+        fs.readFileSync(`${cherryDir}/${file}`),
+        { tagIds: [cherryTag.id] }
+      )
+    );
+  });
+
+  await Promise.all(fileUploadPromises);
+
+  console.log("Training...");
+  let trainingIteration = await trainer.trainProject(sampleProject.id);
+
+  // Wait for training to complete
+  console.log("Training started...");
+  while (trainingIteration.status == "Training") {
+    console.log("Training status: " + trainingIteration.status);
+    await setTimeoutPromise(1000, null);
+    trainingIteration = await trainer.getIteration(
+      sampleProject.id,
+      trainingIteration.id
+    );
+  }
+  console.log("Training status: " + trainingIteration.status);
+
+  // Publish the iteration to the end point
+  await trainer.publishIteration(
+    sampleProject.id,
+    trainingIteration.id,
+    publishIterationName,
+    predictionResourceId
+  );
+
+  const testFile = fs.readFileSync(`${sampleDataRoot}/Test/test_image.jpg`);
+
+  const results = await predictor.classifyImage(
+    sampleProject.id,
+    publishIterationName,
+    testFile
+  );
+
+  // Show results
+  console.log("Results:");
+  results.predictions.forEach((predictedResult) => {
+    console.log(
+      `\t ${predictedResult.tagName}: ${(
+        predictedResult.probability * 100.0
+      ).toFixed(2)}%`
+    );
+  });
+})();
