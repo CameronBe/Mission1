8be96377cb054c2932d91f6b3d5214775b054102
Autousername <cameron.belcher@developersinstitute.co.nz>
2025-02-26 13:05:52 +1300

    Added a very simple hero section, but decided not to go with a backround image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 import VehicleImageUploader from "./components/vehicleImageUploader/VehicleImageUploader";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Home</h1>
+      <Hero />
       <VehicleImageUploader />
     </div>
   );
